4af0f619a42af026bde381b833a190e95e5c293c
ragingwind <ragingwind@gmail.com>
2016-11-08 13:11:59 +0900

    Fix indents

--- src/routes.js
+++ src/routes.js
@@ -7,18 +7,18 @@ import Main from './components/Main';
 module.exports = <Route path="/" component={App}>
 	<IndexRoute component={Main}/>
 	<Route path="users" getComponent={(s, cb) => {
-			System.import('./components/Users').then((component) => {
-				cb(null, component.default || component);
-			});
-		}}/>
-		<Route path="users/:id" getComponent={(s, cb) => {
-				System.import('./components/Users').then((component) => {
-					cb(null, component.default || component);
-				});
-			}}/>
-			<Route path="contact" getComponent={(s, cb) => {
-					System.import('./components/Contact').then((component) => {
-						cb(null, component.default || component);
-					});
-				}}/>
+		System.import('./components/Users').then((component) => {
+			cb(null, component.default || component);
+		});
+	}}/>
+	<Route path="users/:id" getComponent={(s, cb) => {
+		System.import('./components/Users').then((component) => {
+			cb(null, component.default || component);
+		});
+	}}/>
+	<Route path="contact" getComponent={(s, cb) => {
+		System.import('./components/Contact').then((component) => {
+			cb(null, component.default || component);
+		});
+	}}/>
 </Route>
